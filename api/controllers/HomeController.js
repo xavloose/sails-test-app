3a1cbe4ded8e1dc25e4549e21ccbd2d4b9ce4a1d
MennoVanDiermen <msvandiermen@gmail.com>
2017-10-21 00:15:29 -0500

    Results page & controller implementation

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -5,17 +5,56 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Promise = require('bluebird');
+
 module.exports = {
 	
-
+  
 
   /**
    * `HomeController.index()`
    */
   index: function (req, res) {
-    return res.json({
-      todo: 'index() is not implemented yet!'
+    let data = {};
+    data.entries = 123456;
+    data.title = "Welcome to READS";
+    return res.view('homepage.ejs', data);
+  },
+  
+  search: function (req, res) {
+    let data = {};
+    proms = [];
+    proms.push(new Promise(function (pass, fail) {
+      tools = [];
+      for(let i=0;i<10;i++) {
+        tool = {};
+        tool.id = i;
+        tool.name = "Tool"+i;
+        tool.version = "0."+i;
+        tools.push(tool);
+      };
+      data.tools = tools;
+      pass();
+    }));
+    proms.push(new Promise(function (pass, fail) {
+      imgs = [];
+      for(let i=0;i<10;i++) {
+        img = {};
+        img.id = i;
+        img.name = "Image"+i;
+        img.version = "0."+i;
+        imgs.push(img);
+      };
+      data.imgs = imgs;
+      pass();
+    }));
+    Promise.all(proms).then(function() {
+      data.title = "Results";
+      data.query = req.param('query');
+      return;
+    })
+    .then(function() {
+      return res.view('results.ejs', data);
     });
   }
 };
-
